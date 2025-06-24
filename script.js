e189954c6729460200dc7a7432437abf67eedbf4
Lidiane Yumi <127134394+proflidianemd@users.noreply.github.com>
2025-06-24 14:32:17 -0300

    .

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultados");
-const textoResultado = document.querySelector(".texto-resultados");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
